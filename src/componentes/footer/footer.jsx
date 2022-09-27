06f70062676997a2b00a19e0112ac7abd12260a2
omarurregodev <omarurrego07@gmail.com>
2022-09-27 14:35:07 -0500

    run build

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="lg:text-left">
             <div className="text-darkWhite text-center p-3" style={styles.bgFooter}>
-                © 2022 Copyright, hecho por <a href="https://www.linkedin.com/in/omar-torres-u/" className='text-darkWhite hover:text-white'>Omar Torres Urrego.</a> 
+                © 2022 Copyright, hecho por <a href="https://www.linkedin.com/in/omar-torres-u/" className='text-darkWhite hover:text-white' target="blank">Omar Torres Urrego.</a> 
             </div>
         </footer>
     );
