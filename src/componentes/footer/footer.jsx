f69944e60dab8db9d56fdc8db5fbac6130d60e10
omarurregodev <omarurrego07@gmail.com>
2022-09-27 21:53:46 -0500

    modificacion linkedin

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="lg:text-left">
             <div className="text-darkWhite text-center p-3" style={styles.bgFooter}>
-                © 2022 Copyright, hecho por <a href="https://www.linkedin.com/in/juliana-tamayo-laverde/" className='text-darkWhite hover:text-white' target="blank">Juliana Tamayo.</a> 
+                © 2022 Copyright, hecho por <a href="https://www.linkedin.com/in/juliana-tamayo-laverde/" className='text-darkWhite hover:text-blue' target="blank">Juliana Tamayo.</a> 
             </div>
         </footer>
     );
